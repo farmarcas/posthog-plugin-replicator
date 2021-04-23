9998e0b068b75e124d1c3ab31d5b9a754706832e
Michael Matloka <dev@twixes.com>
2021-04-23 11:40:35 +0200

    Batch events

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+import { createBuffer } from '@posthog/plugin-contrib'
 import { Plugin, PluginMeta } from '@posthog/plugin-scaffold'
 import fetch from 'node-fetch'
 
 export interface ReplicatorMeta extends PluginMeta {
+    global: {
+        buffer: ReturnType<typeof createBuffer>
+    }
     config: {
         host: string
         project_api_key: string
@@ -10,14 +14,32 @@ export interface ReplicatorMeta extends PluginMeta {
 }
 
 const plugin: Plugin<ReplicatorMeta> = {
-    processEvent: async (event, { config }) => {
+    setupPlugin: ({ global, config }) => {
+        global.buffer = createBuffer({
+            limit: 1024 * 1024, // 1 MB
+            timeoutSeconds: 1,
+            onFlush: async (batch) => {
+                await fetch(`https://${config.host}/e`, {
+                    method: 'POST',
+                    body: JSON.stringify(batch),
+                    headers: { 'Content-Type': 'application/json' },
+                })
+            },
+        })
+    },
+
+    teardownPlugin: ({ global }) => {
+        global.buffer.flush()
+    },
+
+    processEvent: async (event, { config, global }) => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { team_id, now, offset, ...sendableEvent } = { ...event, token: config.project_api_key }
-        await fetch(`https://${config.host}/e`, {
-            method: 'POST',
-            body: JSON.stringify(Array(parseInt(config.replication) || 1).fill(sendableEvent)),
-            headers: { 'Content-Type': 'application/json' },
-        })
+        const sendableEventSize = JSON.stringify(sendableEvent).length
+        const replication = parseInt(config.replication) || 1
+        for (let i = 0; i < replication; i++) {
+            global.buffer.add(sendableEvent, sendableEventSize)
+        }
         return event
     },
 }
