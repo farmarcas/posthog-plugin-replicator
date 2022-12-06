fc42f775633892784a973b63e058702073a832bf
Ben White <ben@posthog.com>
2022-12-06 14:26:52 +0100

    Added autocapture reversing with a test

--- index.ts
+++ index.ts
@@ -9,15 +9,40 @@ export interface ReplicatorMetaInput {
     }
 }
 
+const reverseAutocaptureEvent = (autocaptureEvent: any) => {
+    // TRICKY: This code basically reverses what the plugin server does
+    // Adapted from https://github.com/PostHog/posthog/blob/master/plugin-server/src/utils/db/elements-chain.ts#L105
+    const { elements, properties, ...event } = autocaptureEvent
+
+    const $elements = elements.map((el: any) => {
+        const { attributes, text,  ...commonProps } = el
+        return {
+            ...commonProps,
+            $el_text: text,
+            ...attributes
+        }
+    })
+
+    return {
+        ...event,
+        properties: {
+            ...properties,
+            $elements: $elements,
+        },
+    }
+}
+
 const plugin: Plugin<ReplicatorMetaInput> = {
     exportEvents: async (events, { config }) => {
         const batch = []
         for (const event of events) {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { team_id, now, offset, sent_at, $token, project_id, api_key, ...sendableEvent } = { ...event, token: config.project_api_key }
+            const { team_id, now, offset, sent_at, $token, project_id, api_key, ...sendableEvent } = { ...event, token: config.project_api_key } as any
             const replication = parseInt(config.replication) || 1
+            const eventToSend = (sendableEvent.event === '$autocapture') ? reverseAutocaptureEvent(sendableEvent) : sendableEvent
+
             for (let i = 0; i < replication; i++) {
-                batch.push(sendableEvent)
+                batch.push(eventToSend)
             }
         }
 
